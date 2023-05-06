c361635849ff5eee7e2ef455cda5a4e363f9d2ab
airpoland <77587515+airpoland@users.noreply.github.com>
2023-05-06 17:58:43 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Header from './UI/Header';
+
+const DUMMY_MEALS = [
+	{
+		id: 'm1',
+		name: 'Sushi',
+		description: 'Finest fish and veggies',
+		price: 22.99,
+	},
+	{
+		id: 'm2',
+		name: 'Schnitzel',
+		description: 'A german specialty!',
+		price: 16.5,
+	},
+	{
+		id: 'm3',
+		name: 'Barbecue Burger',
+		description: 'American, raw, meaty',
+		price: 12.99,
+	},
+	{
+		id: 'm4',
+		name: 'Green Bowl',
+		description: 'Healthy...and green...',
+		price: 18.99,
+	},
+];
+
+function App() {
+	return (
+		<React.Fragment>
+			<Header></Header>
+			<div>
+				<h2>Let's get started!</h2>
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default App;
